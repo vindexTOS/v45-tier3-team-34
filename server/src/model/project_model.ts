a659354044d856654e9e69448e8a2ed51492b22c
VindexTOS <giorgikutateladze1998@gmail.com>
2023-08-09 21:18:08 +0400

    basic mongo db model schema setup done

--- server/src/model/project_model.ts
+++ server/src/model/project_model.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
 
 const Project_model = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: [true, 'title is required'],
@@ -13,4 +17,28 @@ const Project_model = new mongoose.Schema({
     type: String,
     required: [true, 'photo is reuqired'],
   },
+  github: {
+    type: String,
+  },
+  liveLink: {
+    type: String,
+  },
+  videoLink: {
+    type: String,
+  },
+  skills_used: [
+    {
+      _id: mongoose.Schema.Types.ObjectId,
+      title: {
+        type: String,
+        required: true,
+      },
+      icon: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 })
+
+export default mongoose.model('connect_dev_user_projects', Project_model)
