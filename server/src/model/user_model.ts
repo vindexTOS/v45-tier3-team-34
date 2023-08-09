31275d9a423a27d4c194b39d16419c3b9fbd2fb3
VindexTOS <giorgikutateladze1998@gmail.com>
2023-08-09 21:03:46 +0400

    init commint with some models to back_end branch

---
+++ server/src/model/user_model.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+
+const User_model = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: [true, 'user name is requried'],
+  },
+  emaiL: {
+    type: String,
+    required: [true, 'email is requried'],
+  },
+
+  password: {
+    type: String,
+    required: [true, 'password is required'],
+  },
+
+  avatar: {
+    type: String,
+    default: `https://img.freepik.com/free-icon/man_318-677829.jpg?w=360`,
+  },
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+  role: {
+    type: String,
+    enum: ['developer', 'startup', 'company', 'HR'],
+    default: 'developer',
+  },
+  skill_id: {
+    type: String,
+  },
+  projects_id: {
+    type: String,
+  },
+  task_id: {
+    type: String,
+  },
+  review_id: {
+    type: String,
+  },
+})
+
+export default mongoose.model('connect-dev-user', User_model)
