141224e3b244f224713fa4ea0b64d31859d36ad0
edriso <edriso.dev@gmail.com>
2023-08-16 14:25:56 +0300

    add project CRUD methods

--- server/src/controller/project-controller.ts
+++ server/src/controller/project-controller.ts
@@ -11,3 +11,34 @@ export const createProject = tryCatch(async (req: Request, res: Response) => {
   const newProject = await Project_model.create(req.body);
   res.status(201).json({ project: newProject });
 });
+
+export const getProject = tryCatch(async (req: Request, res: any) => {
+  const projectId = req.params.id;
+  const project = await Project_model.findById(projectId);
+  if (!project) {
+    return res.status(404).json({ message: 'Project not found' });
+  }
+  res.status(200).json({ project });
+});
+
+export const updateProject = tryCatch(async (req: Request, res: any) => {
+  const projectId = req.params.id;
+  const updatedProject = await Project_model.findByIdAndUpdate(
+    projectId,
+    req.body,
+    { new: true },
+  );
+  if (!updatedProject) {
+    return res.status(404).json({ message: 'Project not found' });
+  }
+  res.status(200).json({ project: updatedProject });
+});
+
+export const deleteProject = tryCatch(async (req: Request, res: any) => {
+  const projectId = req.params.id;
+  const deletedProject = await Project_model.findByIdAndDelete(projectId);
+  if (!deletedProject) {
+    return res.status(404).json({ message: 'Project not found' });
+  }
+  res.status(204).send();
+});
