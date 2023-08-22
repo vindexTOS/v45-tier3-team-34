3549f4923fefd61079d239ef12f573c4435b3231
Daurius Swarts <ali@openmindi.co.za>
2023-08-22 16:14:20 +0200

    [FE] Mega Footer Template complete

--- client/src/contants/index.ts
+++ client/src/contants/index.ts
@@ -126,4 +126,69 @@ export const footerMenuItems = [
         } 
       ]
     },
+    {
+      name: "Find Developer",
+      subMenus: [
+        {
+          title: "List All Projects",
+          description: "See a list of all available Projects",
+          link: "#"
+        },
+        {
+          title: "Resource Tools",
+          description: "Follow these easy steps to get started",
+          link: "#"
+        },
+        {
+          title: "View my Projects",
+          description: "Browse our list of all Active/Completed Projects",
+          link: "#"
+        },
+        {
+          title: "Need Help",
+          description: "Feel free to contact us for more info",
+          link: "#"
+        } 
+      ]
+    },
+    {
+      name: "Why DevConnect",
+      subMenus: [
+        {
+          title: "About us",
+          description: "Read more about the project vision and team info",
+          link: "#"
+        },
+        {
+          title: "Success Stories",
+          description: "Read on successfully completed projects",
+          link: "#"
+        },
+        {
+          title: "FAQs",
+          description: "Browse our list of Frequently Asked Questions",
+          link: "#"
+        }
+      ]
+    },
+    {
+      name: "More Info",
+      subMenus: [
+        {
+          title: "Develpment Team",
+          description: "See a comprehensive list of all our junior developers",
+          link: "#"
+        },
+        {
+          title: "Vision Statement",
+          description: "Browse our list of all Completed Projects",
+          link: "#"
+        },
+        {
+          title: "Contact us",
+          description: "Feel free to contact us for more info",
+          link: "#"
+        } 
+      ]
+    },
 ]
