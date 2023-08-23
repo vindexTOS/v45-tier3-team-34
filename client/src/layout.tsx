ee7094ae8b0b55fd0cde53edda25adaa1663b49d
varchodi <rauschenrobert@gmail.com>
2023-08-23 13:35:32 +0000

    tailwind refracted by preserving it colors

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({
   children: ReactElement;
 }) => {
   return (
-    <div className="sm:px-1 md:px-8 md:py-2 bg-gradient-to-br from-gray-200 via-gray-300 to-gray-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 flex flex-col justify-between transition-all duration-700 delay-200 ease-in-out">
+    <div className="sm:px-1 md:px-8 md:py-2 bg-gradient-to-br from-gray-200 via-gray-300 to-gray-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 flex flex-col justify-between transition-all duration-700 delay-200 ease-in-out ">
       <NavBar />
       {children}
       <Footer />
