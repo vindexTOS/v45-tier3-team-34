8df761650571477a559b39ca3bfab52765ae14da
Daurius Swarts <ali@openmindi.co.za>
2023-08-24 20:15:09 +0200

    [FE] Refactor Hero Section

--- client/src/components/Home/Hero/HeroSection.tsx
+++ client/src/components/Home/Hero/HeroSection.tsx
@@ -1,16 +1,18 @@
 import { companies } from "../../../contants";
 import HeroCTAButton from "../../Buttons/HeroCTAButton";
 import HeroCompany from "./HeroCompany";
+import Lottie from "lottie-react";
+import animationData from "../../../../public/assets/lottie/animation_llpgw5p0.json";
 
 const HeroSection = () => {
   return (
-    <section className="bg-gray-100 dark:bg-slate-900 rounded-lg w-full flex flex-col sm:flex-row  transition-all duration-500 delay-100 ease-in-out">
+    <section className="rounded-lg w-full flex flex-col sm:flex-row transition-all duration-500 delay-100 ease-in-out">
       <article className="flex-1 flex flex-col items-start justify-center gap-20 p-5 lg:px-20">
         <div className="flex flex-col gap-2 lg:gap-4">
-          <h1 className="text-3xl lg:text-6xl capitalize font-bold text-slate-800 dark:text-slate-300">
-            DevConnect
+          <h1 className="text-3xl lg:text-6xl capitalize font-bold text-light-text dark:text-dark-text">
+            DevConnect X
           </h1>
-          <p className="text-base font-normal text-green-900 dark:text-slate-400">
+          <p className="text-base font-normal text-light-text dark:text-dark-text">
             We connect junior developers with{" "}
             <br />
             low-paid real-world projects.
@@ -20,7 +22,7 @@ const HeroSection = () => {
           <div className="flex gap-x-10 gap-y-2 sm:gap-14 w-full sm:w-max flex-wrap mt-5">
             <HeroCTAButton
               title="Hire developers"
-              color="red"
+              color="green"
             />
             <HeroCTAButton
               title="Explore services"
@@ -29,11 +31,11 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <h2 className="text-base font-normal text-green-900 dark:text-slate-400">
+          <h2 className="text-base font-normal text-light-text dark:text-dark-text">
             Working with the best
           </h2>
           {/* socials */}
-          <div className="flex gap-8 text-green-900 dark:text-slate-400">
+          <div className="flex gap-8 text-light-text dark:text-dark-text">
             {companies.map(({ name, icon }) => (
               <HeroCompany
                 key={name}
@@ -45,11 +47,10 @@ const HeroSection = () => {
         </div>
       </article>
       {/* Image section */}
-      <article className="flex-1">
-        <img
-          className=""
-          src="assets/img/hero-image.svg"
-          alt="hero"
+      <article className="flex-1 flex items-center justify-center">
+        <Lottie
+          className="w-[80%] h-auto"
+          animationData={animationData}
         />
       </article>
     </section>
