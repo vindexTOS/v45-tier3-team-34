009922769c70af8453f16b5821f5f00b1a31ab4c
Daurius Swarts <ali@openmindi.co.za>
2023-08-25 20:31:54 +0200

    [FE] Updated SignUp page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -54,104 +54,99 @@ const Register = () => {
   }
 
   return (
-    <main className="w-full flex flex-col md:flex-row place-content-center ">
+    <main className="flex items-center justify-center relative mb-20 -mt-20">
       <Error error={statusState.error} />
       <Loading loading={Authloading} />
-      {/* <button onClick={() => console.log(ImgState.imgUrl)}>CLIC</button> */}
-      <section className="w-full md:w-[50%] lg:w-[40%] flex flex-col  py-4 px-2 sm:px-12 ">
-        {/* kinda logo */}
-        <article className="text-gray-800 dark:text-gray-400">
-          <a
-            href="/"
-            className="text-xg md:text-2xl font-extrabold "
-          >
-            devConnect
-          </a>
-          <h1 className="text-sm  font-extralight">
-            Description goes here
+      {/* <button onClick={() => console.log(ImgState.imgUrl)}>CLICK</button> */}
+      <section className="w-[90%] lg:w-[30%] h-[80vh] flex flex-col py-10 px-5 sm:px-12 text-center border border-slate-500/50 dark:border-green-500/50 bg-white rounded-2xl dark:bg-slate-800 justify-between">
+        {/* Title */}
+        <article className="">
+          <h1 className="text-sm font-sm text-light-text dark:text-dark-text">
+            Hello and Welcome to
           </h1>
+          <Link
+            to="/"
+            className="text-2xl font-extrabold text-green-700 dark:text-green-500"
+          >
+            DevConnect
+          </Link>
         </article>
 
         {/* title */}
-        <article className="my-6 sm:my-8">
-          <h1 className="text-3xl md:text-2xl dark:text-gray-400 font-light md:font-semibold">
+        <article className="">
+          <h1 className="text-[1.4rem] text-gray-600 dark:text-dark-text font-light">
             Create a free account
           </h1>
         </article>
         {/* form here  */}
-        <article className="flex flex-col gap-y-2 xl:mr-20">
+        <article className="flex flex-col gap-y-2 justify-center items-center">
           {/* upload image need more style improvement */}
           <div className="relative">
             <ImgUpload avatar="" />
           </div>
+          <div className="space-y-3 w-[80%]">
+            <InputField
+              data={formData}
+              setData={setFormData}
+              type="text"
+              field="full_name"
+              label="Full Name"
+            />
+            <DropDownSelect
+              setData={setFormData}
+              type="text"
+              field="role"
+              label="Who are you ?"
+            />
+            <InputField
+              data={formData}
+              setData={setFormData}
+              field="email"
+              type="email"
+              label="Email"
+            />
 
-          <InputField
-            data={formData}
-            setData={setFormData}
-            type="text"
-            field="full_name"
-            label="Full Name"
-          />
-          <DropDownSelect
-            setData={setFormData}
-            type="text"
-            field="role"
-            label="Who are you ?"
-          />
-          <InputField
-            data={formData}
-            setData={setFormData}
-            field="email"
-            type="email"
-            label="Email"
-          />
-
-          <InputField
-            field="password"
-            type="password"
-            label="Password"
-            data={formData}
-            setData={setFormData}
-          />
-          <InputField
-            field="password_confirm"
-            type="password"
-            label="confirm password"
-            data={formData}
-            setData={setFormData}
-          />
+            <InputField
+              field="password"
+              type="password"
+              label="Password"
+              data={formData}
+              setData={setFormData}
+            />
+            <InputField
+              field="password_confirm"
+              type="password"
+              label="confirm password"
+              data={formData}
+              setData={setFormData}
+            />
+          </div>
 
           {/* create account button */}
+        </article>
+        <article>
+          <div className="my-4 text-center flex items-center justify-center">
+            <div className="border-t border-gray-200 flex-grow"></div>
+            <Link
+              className="font-light text-[13px] text-gray-500 dark:text-gray-300 mx-2 p-2"
+              to="/login"
+            >
+              Already have an account
+            </Link>
+            <div className="border-t border-gray-200 flex-grow"></div>
+          </div>
+
           <button
-            //submit data
             onClick={handleSubmit}
-            className="mt-2 bg-red-600 py-4 px-4 outline-none border-none rounded-sm font-semibold text-sm text-white"
+            className="mt-2 mx-auto py-2 px-16 border-2 border-green-700 rounded-2xl font-semibold text-base text-green-700 hover:bg-green-400/30  hover:border-green-600 transition-all duration-300"
           >
-            create account
+            Create account
           </button>
-          <Link
-            className="font-light text-end text-[13px] text-gray-700"
-            to="/login"
-          >
-            Already have an account
-          </Link>
         </article>
       </section>
-      {/* was hidden */}
-      <section className=" flex-1 md:block   md:min-h-screen bg-[url('/assets/img/bg-temp.jpg')] bg-cover bg-no-repeat bg-center">
-        {/* image or samething else */}
-        <article className="bg-gradient-to-b h-[400px] md:min-h-full from-slate-800 to-transparent p-4">
-          <h1 className="text-4xl font-extrabold text-white ">
-            Add samething here ...
-          </h1>
-          <p className="text-lg text-white font-light">
-            Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Nesciunt
-            ullam odio deserunt ipsum, modi,
-            soluta nulla
-          </p>
-        </article>
-      </section>
+      <span className="absolute -bottom-10 left-0 right-0 whitespace-nowrap text-xs text-slate-500 text-center justify-center">
+        © 2023 DevConnect
+      </span>
     </main>
   );
 };
