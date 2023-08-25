95f9eae229b8309e77e5b09dfc681c05a884fab8
Daurius Swarts <ali@openmindi.co.za>
2023-08-25 19:37:55 +0200

    X [FE] Fixed Login Page

--- client/src/components/Forms/InputField.tsx
+++ client/src/components/Forms/InputField.tsx
@@ -1,50 +1,84 @@
-import { HTMLInputTypeAttribute, useEffect } from 'react'
-import { RegisterFormType } from '../../common.types'
+import {
+  HTMLInputTypeAttribute,
+  useEffect,
+} from "react";
+import { RegisterFormType } from "../../common.types";
 
-type FieldType = 'full_name' | 'email' | 'password' | 'password_confirm'
+type FieldType =
+  | "full_name"
+  | "email"
+  | "password"
+  | "password_confirm";
 
 type Proptypes = {
-  label: string
-  type: HTMLInputTypeAttribute
-  field: FieldType
-  setData: React.Dispatch<React.SetStateAction<RegisterFormType>>
-  data?: RegisterFormType
-}
-const InputField = ({ label, setData, type, field }: Proptypes) => {
+  label: string;
+  type: HTMLInputTypeAttribute;
+  field: FieldType;
+  setData: React.Dispatch<
+    React.SetStateAction<RegisterFormType>
+  >;
+  data?: RegisterFormType;
+};
+const InputField = ({
+  label,
+  setData,
+  type,
+  field,
+}: Proptypes) => {
   //changing function
-  function handleChange({ field, value }: { field: FieldType; value: string }) {
+  function handleChange({
+    field,
+    value,
+  }: {
+    field: FieldType;
+    value: string;
+  }) {
     switch (field) {
-      case 'full_name':
-        setData((usr) => ({ ...usr, userName: value }))
-        break
-      case 'email':
-        setData((usr) => ({ ...usr, email: value }))
-        break
-      case 'password':
-        setData((usr) => ({ ...usr, password: value }))
-        break
-      case 'password_confirm':
-        setData((usr) => ({ ...usr, confirmPassword: value }))
-        break
+      case "full_name":
+        setData((usr) => ({
+          ...usr,
+          userName: value,
+        }));
+        break;
+      case "email":
+        setData((usr) => ({
+          ...usr,
+          email: value,
+        }));
+        break;
+      case "password":
+        setData((usr) => ({
+          ...usr,
+          password: value,
+        }));
+        break;
+      case "password_confirm":
+        setData((usr) => ({
+          ...usr,
+          confirmPassword: value,
+        }));
+        break;
 
       default:
-        break
+        break;
     }
   }
 
   return (
-    <div className="w-full ">
-      <h1 className="mb-1">{label}</h1>
+    <div className="w-full">
       <input
         onChange={(e) => {
-          handleChange({ field, value: e.target.value })
+          handleChange({
+            field,
+            value: e.target.value,
+          });
         }}
-        className="border border-gray-400 dark:bg-transparent dark:text-gray-200 outline-green-700 outline-1 w-full py-2 px-4 rounded-sm font-light text-lg"
+        className="border border-gray-400 dark:bg-transparent dark:text-gray-200 outline-green-700 outline-1 w-full py-2 px-4 rounded-full font-light text-sm"
         type={type}
         placeholder={label}
       />
     </div>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
