b6e01ea7c21331fc85f1ef733c43b327962110c9
Daurius Swarts <ali@openmindi.co.za>
2023-08-25 16:59:26 +0200

    [FE] Fixed Browse projects

--- client/src/components/Home/CategoryBrowse/CategoryCard.tsx
+++ client/src/components/Home/CategoryBrowse/CategoryCard.tsx
@@ -14,12 +14,12 @@ const CategoryCard = ({
   return (
     <Link
       to={link}
-      className="bg-gray-200 backdrop-blur-sm  dark:bg-white/10 dark:text-gray-300 rounded-lg p-4 md:p-8 shadow-md dark:shadow-gray-950 hover:shadow-2xl transition-shadow delay-75 min-h-[120px] flex flex-col justify-between cursor-pointer"
+      className="bg-[#F7FAF7] backdrop-blur-sm  dark:bg-white/10 dark:text-gray-400 rounded-lg p-4 md:p-8 hover:shadow-md dark:shadow-gray-950 hover:bg-[#eff9ef] dark:hover:bg-white/5 min-h-[120px] flex flex-col justify-between cursor-pointer border border-[#e1e6e1]  text-slate-700 dark:border-green-900"
     >
-      <h1 className="text-xl md:text-2xl 2xl:text-3xl font-semibold mb-2">
+      <h1 className="text-[1.2rem] font-semibold mb-2">
         {title}
       </h1>
-      <div className="flex justify-between text-slate-500">
+      <div className="flex justify-between text-[0.9rem] text-green-900 dark:text-green-700">
         <p className="flex items-center gap-1 ">
           <span className="text-green-600 text-xl">
             <HiStar />
