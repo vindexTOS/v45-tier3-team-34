7932af9aee619f1389fadbedcd3a4873c9470a8b
Daurius Swarts <ali@openmindi.co.za>
2023-08-28 12:19:03 +0200

    [FE] Moved MainMenu to NavBar Folder

--- client/src/components/NavBar/MainMenu/FindCompany/Developer_list.tsx
+++ client/src/components/NavBar/MainMenu/FindCompany/Developer_list.tsx
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { UseMainContext } from '../../../context'
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { UseMainContext } from "../../../../context";
 const Developer_list = () => {
-  const { GetSingleDev } = UseMainContext()
-  const [devData, setDevData] = useState<any>()
-  const navigate = useNavigate()
+  const { GetSingleDev } = UseMainContext();
+  const [devData, setDevData] = useState<any>();
+  const navigate = useNavigate();
   const GetAllDevs = async () => {
     try {
-      const res = await axios.get(`http://localhost:8080/all_devs`)
+      const res = await axios.get(
+        `http://localhost:8080/all_devs`
+      );
 
-      setDevData(res.data)
-      console.log(res.data)
+      setDevData(res.data);
+      console.log(res.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    GetAllDevs()
-  }, [])
+    GetAllDevs();
+  }, []);
   if (devData && devData.devs) {
     return (
       <div className="mt-60 w-[100vw] items-center justify-center flex flex-wrap gap-3">
         {devData.devs.map((val: any) => {
-          const { avatar, date, email, role, userName, _id } = val.user
+          const {
+            avatar,
+            date,
+            email,
+            role,
+            userName,
+            _id,
+          } = val.user;
           //   const { hrPay, firstName, lastName, userTimeZone, title } =
           //     val.user_info || {}
 
@@ -42,18 +54,24 @@ const Developer_list = () => {
                 />
               </div>
               <div className="mt-4">
-                <h2 className="text-lg font-semibold">{userName}</h2>
-                <p className="text-gray-500">{role}</p>
+                <h2 className="text-lg font-semibold">
+                  {userName}
+                </h2>
+                <p className="text-gray-500">
+                  {role}
+                </p>
                 {/* Add more information here */}
               </div>
             </div>
-          )
+          );
         })}
       </div>
-    )
+    );
   } else {
-    return <div className="mt-60">Loading...</div>
+    return (
+      <div className="mt-60">Loading...</div>
+    );
   }
-}
+};
 
-export default Developer_list
+export default Developer_list;
