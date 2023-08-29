1fd8a4d5a1d34aea041ed501314840e3b3494200
Daurius Swarts <ali@openmindi.co.za>
2023-08-29 22:33:17 +0200

    [FE] Moved Folders and Added About Page

--- client/src/pages/MainMenu/FindDeveloper/NeedHelpDev.tsx
+++ client/src/pages/MainMenu/FindDeveloper/NeedHelpDev.tsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
-import NeedHelpCard from "../../../FindCompanyPage/NeedHelpCard";
+// import React from "react";
 
-export default function NeedHelp() {
+import { Link } from "react-router-dom";
+import NeedHelpDevCard from "../../../components/MainMenuPages/FindDeveloperPage/NeedHelpDevCard";
+
+export default function NeedHelpDev() {
   return (
     <div className="w-[80%] mx-auto">
       <div className="bg-[url('/assets/img/find_client_bg.png')] bg-cover  bg-no-repeat bg-gray-500 bg-fixed md:bg-scroll   sm:rounded-xl  text-white p-2 md:p-8 flex flex-col gap-2 md:gap-y-20 mx-auto">
         <section className="flex flex-col gap-4 md:gap-10">
-          <article className="w-full md:max-w-xl flex flex-col gap-y-12 bg-blue-700 rounded-lg p-5">
-            <h1 className="text-xs capitalize font-light py-1 px-4 bg-blue-900 rounded-lg max-w-min whitespace-nowrap">
-              Find Company
+          <article className="w-full md:max-w-xl flex flex-col gap-y-12 bg-green-600 rounded-lg p-5">
+            <h1 className="text-xs capitalize font-light py-1 px-4 bg-green-800 rounded-lg max-w-min whitespace-nowrap">
+              Find Developer
             </h1>
             <h1 className="text-[2.8rem] font-bold ">
-              Need Help?
+              Need Developer Help?
             </h1>
             <p className="text-[1.2rem] font-normal">
               Follow these easy steps to begin{" "}
@@ -35,34 +37,34 @@ export default function NeedHelp() {
       <div>
         {/* actions */}
         <article className="flex flex-wrap gap-6 space-y-5 sm:space-y-0 items-center justify-center my-20">
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Create company profile"
+            title="Create a profile"
             sub_title="Highlight your skills and experience, show your portfolio, and set your ideal pay rate."
           />
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Post a Project"
+            title="Search for jobs"
             sub_title="Search on Talent Marketplace™ for the hourly or fixed-price work you’re looking for."
           />
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Review Applications"
+            title="Submit a proposal"
             sub_title="Set your rate and tell clients why you’re the right person for the job!"
           />
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Assign Developer"
+            title="Get contract"
             sub_title="If the client likes your proposal they’ll send you a contract to begin working."
           />
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Approve work"
+            title="Complete the work"
             sub_title="Check steps off as you finish and work with your client if you have questions."
           />
-          <NeedHelpCard
+          <NeedHelpDevCard
             link="/"
-            title="Pay Develpor"
+            title="Get paid securely"
             sub_title="Once the client approves your work, you'll get paid and they can leave you feedback."
           />
         </article>
