60e51556723b6b5977609fbdaf3e42c32984ca61
varchodi <rauschenrobert@gmail.com>
2023-08-30 18:54:57 +0300

    implement company project page ?? strill ongoing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,27 +11,6 @@ import Portfolio_Preview from './components/Dev_Portfolio/Portfolio_Preview'
 
 import Layout from './layout'
 
-import PostProject from './pages/MainMenu/FindCompany/PostProject'
-import ViewProjects from './pages/MainMenu/FindCompany/ViewProjects'
-import NeedHelp from './pages/MainMenu/FindCompany/NeedHelp'
-import NeedHelpDev from './pages/MainMenu/FindDeveloper/NeedHelpDev'
-import MyProjects from './pages/MainMenu/FindDeveloper/MyProjects'
-import ResourceTools from './pages/MainMenu/FindDeveloper/ResourceTools'
-import ListProjects from './pages/MainMenu/FindDeveloper/ListProjects'
-import FAQs from './pages/MainMenu/WhyDevConnect/FAQs'
-import SuccessStories from './pages/MainMenu/WhyDevConnect/SuccessStories'
-import AboutUs from './pages/MainMenu/WhyDevConnect/AboutUs'
-
-<<<<<<< HEAD
-import Single_User_Page from './pages/Profiles/Single_User_Page'
-import User_Portfolio_Single from './components/User/User_Portfolio_Single'
-import User_info_form from './pages/Forms/User_info_form'
-import Developer_list from './pages/MainMenu/FindCompany/Developer_list'
-import FindCompanyMain from './pages/MainMenu/FindCompany/FindCompanyMain'
-import Company_info_form from './pages/Forms/Company_info_form'
-import Company_Profile from './pages/Profiles/Company_Profile'
-import CompanyProjectForm from './pages/Forms/Company_Project_posting'
-=======
 import PostProject from "./pages/MainMenu/FindCompany/PostProject";
 import ViewProjects from "./pages/MainMenu/FindCompany/ViewProjects";
 import NeedHelp from "./pages/MainMenu/FindCompany/NeedHelp";
@@ -52,8 +31,8 @@ import Company_info_form from "./pages/Forms/Company_info_form";
 import Company_Profile from "./pages/Profiles/Company_Profile";
 import ProjectsListingPage from "./pages/ProjectsListingPage";
 import Project_Page from "./pages/Project/Project_Page";
+import CompanyProjectForm from './pages/Forms/Company_Project_posting'
 
->>>>>>> f6f329ce5f60d6c03414372f9a489f85a409ebbd
 //routes
 const router = [
   {
@@ -169,13 +148,6 @@ const router = [
     element: <Single_User_Page />,
   },
 
-<<<<<<< HEAD
-  //projects page (by category) !! ??
-  // {
-  //   path: '/projects/:project_category',
-  //   element:<ProjectsPage/>
-  // }
-=======
 
   //projects listing page (by category) !! ??
   {
@@ -187,7 +159,6 @@ const router = [
     path: '/company/projects/:project_id',
     element:<Project_Page/>
   },
->>>>>>> f6f329ce5f60d6c03414372f9a489f85a409ebbd
 ]
 
 type ReactRouteType = {
