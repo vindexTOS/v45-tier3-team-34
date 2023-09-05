27203d1e5b9240db20e72cae92ac24dcd912448f
varchodi <rauschenrobert@gmail.com>
2023-09-05 07:11:44 +0300

    update apply btn , add chat link btn, and company

--- client/src/components/Project_detail/Tier_details.tsx
+++ client/src/components/Project_detail/Tier_details.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { tierCategoryType } from '../../common.types'
 import { tiers } from '../../pages/Project/dummy_project'
+import { RxCrossCircled } from "react-icons/rx";
+import { BsCheckCircle } from 'react-icons/bs';
 
 const TierDetails = ({ tier }: { tier: tierCategoryType | any }) => {
   const tier_data = tiers.filter((t) => t.type === tier)[0]
@@ -51,7 +53,13 @@ const TierDetails = ({ tier }: { tier: tierCategoryType | any }) => {
         <h1 className="text-xl font-thin">Design Customization</h1>
         <p className="font-bold text-green-800 dark:text-green-200 text-xl">
           {' '}
-          {DesignCustomization ? '✅' : '❌'}
+          {DesignCustomization ? 
+            <p className='text-green-500 text-xl font-bold'>
+            <BsCheckCircle/>
+          </p>
+           : <p className='text-red-500 text-2xl font-bold'>
+            <RxCrossCircled/>
+          </p>}
         </p>
       </article>
 
@@ -59,7 +67,13 @@ const TierDetails = ({ tier }: { tier: tierCategoryType | any }) => {
         <h1 className="text-xl font-thin">Content Upload</h1>
         <p className="font-bold text-green-800 dark:text-green-200 text-xl">
           {' '}
-          {ContentUpload ? '✅' : '❌'}
+          {ContentUpload ?
+            <p className='text-green-500 text-xl font-bold'>
+            <BsCheckCircle/>
+          </p>
+            : <p className='text-red-500 text-2xl font-bold'>
+            <RxCrossCircled/>
+          </p>}
         </p>
       </article>
 
@@ -67,7 +81,13 @@ const TierDetails = ({ tier }: { tier: tierCategoryType | any }) => {
         <h1 className="text-xl font-thin">Responsive Design</h1>
         <p className="font-bold text-green-800 dark:text-green-200 text-xl">
           {' '}
-          {Responsive ? '✅' : '❌'}
+          {Responsive ?
+            <p className='text-green-500 text-xl font-bold'>
+            <BsCheckCircle/>
+          </p>
+            : <p className='text-red-500 text-2xl font-bold'>
+            <RxCrossCircled/>
+          </p>}
         </p>
       </article>
     </div>
