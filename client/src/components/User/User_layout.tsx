58d5897df43ad5c5254a1c6e12b3b34ff9c43fe4
edriso <edriso.dev@gmail.com>
2023-09-08 18:03:23 +0300

    remove a space

--- client/src/components/User/User_layout.tsx
+++ client/src/components/User/User_layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const User_layout = ({ children }: { children: React.ReactNode }) => {
   const style = {
     main: `flex   items-center justify-center mt-2 md:mt-8 `,
-    section: `outline outline-[1px] outline-gray-300 flex flex-col   rounded-[16px] w-full `,
+    section: `outline outline-[1px] outline-gray-300 flex flex-col rounded-[16px] w-full `,
   };
   return (
     <main className={style.main}>
