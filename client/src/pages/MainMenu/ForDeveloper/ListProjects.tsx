a40c1a564ebba523b448a96fceeba71fdfc38ba5
VindexTOS <giorgikutateladze1998@gmail.com>
2023-09-10 23:20:35 +0400

    List Projects

--- client/src/pages/MainMenu/ForDeveloper/ListProjects.tsx
+++ client/src/pages/MainMenu/ForDeveloper/ListProjects.tsx
@@ -1,53 +1,40 @@
 // import React from "react";
 
-import axios from "axios";
-import { UseMainContext } from "../../../context";
-import { useEffect } from "react";
-import { CompanyProjectType } from "../../../common.types";
-
+import axios from 'axios'
+import { UseMainContext } from '../../../context'
+import { useEffect } from 'react'
+import { ProjectCardType } from '../../../common.types'
+import ProjectCard from '../../../components/ProjectsListingPage/Projects/ProjectCard'
 export default function ListProjects() {
-  const {
-    companyProjectsData,
-    setCompanyProjectsData,
-  } = UseMainContext();
+  const { companyProjectsData, setCompanyProjectsData } = UseMainContext()
 
   const getAllProjects = async () => {
     try {
       const res = await axios.get(
-        `${
-          import.meta.env.VITE_GLOBAL_URL
-        }/companies/projects`
-      );
-      setCompanyProjectsData(res.data.companies);
-      console.log(res.data.companies);
+        `${import.meta.env.VITE_GLOBAL_URL}/companies/projects`,
+      )
+      setCompanyProjectsData(res.data.projectsData)
+      console.log(res.data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    getAllProjects();
-  }, []);
+    getAllProjects()
+  }, [])
 
-  if (
-    companyProjectsData &&
-    companyProjectsData.length > 0
-  ) {
+  if (companyProjectsData && companyProjectsData.length > 0) {
     return (
       <div className="text-xl text-center text-light-muted dark:text-dark-muted transition-all duration-500 delay-100 ease-in-out">
-        {companyProjectsData.map(
-          (val: CompanyProjectType) => {
-            return (
-              <div>
-                <h1>{val.title}</h1>
-                <img src={val.image} />
-              </div>
-            );
-          }
-        )}
+        <section className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-between">
+          {companyProjectsData.map((val: ProjectCardType) => {
+            return <ProjectCard data={val} key={val._id} />
+          })}
+        </section>
       </div>
-    );
+    )
   } else {
-    return <div>Loading..</div>;
+    return <div>Loading..</div>
   }
 }
