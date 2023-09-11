7703a034d623f1f7501f83bd67ee0bae5880fd3e
VindexTOS <giorgikutateladze1998@gmail.com>
2023-09-11 23:11:24 +0400

    profile reviews display

--- client/src/pages/Profiles/Company_Profile/Company_Reviews.tsx
+++ client/src/pages/Profiles/Company_Profile/Company_Reviews.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { UseMainContext } from '../../../context'
+import AllRatings from '../AllRatings'
+import { RatingTypes } from '../../../common.types'
 
 const Reviews = () => {
-  return <div>Reviews</div>
+  const { UserState, isUserLoggedIn } = UseMainContext()
+  const [reviewsData, setReviewsData] = useState<RatingTypes[]>([])
+  const GetReviews = async () => {
+    if (isUserLoggedIn) {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_GLOBAL_URL}/rating/${
+            UserState.userData.user._id
+          }`,
+        )
+        setReviewsData(res.data.rating)
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  useEffect(() => {
+    GetReviews()
+  }, [])
+
+  if (isUserLoggedIn && reviewsData && reviewsData.length > 0) {
+    return (
+      <div>
+        <h1>This are your ratings</h1>
+        <AllRatings data={reviewsData} />
+      </div>
+    )
+  } else {
+    return <div>You have no reviews</div>
+  }
 }
 
 export default Reviews
