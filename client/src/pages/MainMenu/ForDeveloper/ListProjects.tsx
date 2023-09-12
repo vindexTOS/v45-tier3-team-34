19c9c61266bd543a6908a270490e5a1c69b784ae
Daurius Swarts <ali@openmindi.co.za>
2023-09-12 16:46:53 +0200

    [FE] Dev page updates

--- client/src/pages/MainMenu/ForDeveloper/ListProjects.tsx
+++ client/src/pages/MainMenu/ForDeveloper/ListProjects.tsx
@@ -5,6 +5,7 @@ import { UseMainContext } from "../../../context";
 import { useEffect } from "react";
 import { ProjectCardType } from "../../../common.types";
 import ProjectCard from "../../../components/ProjectsListingPage/Projects/ProjectCard";
+import DevHeader from "../../../components/MainMenuPages/ForDeveloperPage/DevHeader";
 export default function ListProjects() {
   const {
     companyProjectsData,
@@ -36,8 +37,9 @@ export default function ListProjects() {
     companyProjectsData.length > 0
   ) {
     return (
-      <div className="text-xl text-center text-light-muted dark:text-dark-muted transition-all duration-500 delay-100 ease-in-out">
-        <section className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-between">
+      <div className="">
+        <DevHeader />
+        <section className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-between text-muted dark:text-muted my-20">
           {companyProjectsData.map(
             (val: ProjectCardType) => {
               return (
