9fc210db64a352bdff49590217a69736f7979da3
Daurius Swarts <ali@openmindi.co.za>
2023-09-13 13:45:38 +0200

    [FE] Framer Motion installed

--- client/src/components/Home/Hero/HeroSection.tsx
+++ client/src/components/Home/Hero/HeroSection.tsx
@@ -3,33 +3,43 @@ import HeroCTAButton from "../../Buttons/HeroCTAButton";
 import HeroCompany from "./HeroCompany";
 import Lottie from "lottie-react";
 import animationData from "../../../../src/assets/lottie/animation_llpgw5p0.json";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <section className="rounded-lg w-full flex flex-col sm:flex-row transition-all duration-500 delay-100 ease-in-out">
       <article className="flex-1 flex flex-col items-start justify-center gap-20 p-5 lg:px-20">
-        <div className="flex flex-col gap-2 lg:gap-4">
-          <h1 className="text-[2.7rem] capitalize font-bold text-light-primary dark:text-dark-primary">
-            DevConnect
-          </h1>
-          <p className="text-base font-normal text-light-primary dark:text-dark-primary">
-            We connect junior developers with{" "}
-            <br />
-            low-paid real-world projects
-          </p>
+        <motion.div
+          animate={{ y: 0, opacity: 1 }}
+          initial={{ y: -200, opacity: 0.2 }}
+          transition={{
+            type: "spring",
+            stiffness: 120,
+          }}
+        >
+          <div className="flex flex-col gap-2 lg:gap-4">
+            <h1 className="text-[2.7rem] capitalize font-bold text-light-primary dark:text-dark-primary">
+              DevConnect
+            </h1>
+            <p className="text-base font-normal text-light-primary dark:text-dark-primary">
+              We connect junior developers with{" "}
+              <br />
+              low-paid real-world projects
+            </p>
 
-          {/* actions */}
-          <div className="flex gap-x-10 gap-y-2 sm:gap-14 w-full sm:w-max flex-wrap mt-5">
-            <HeroCTAButton
-              title="Get Started"
-              color="green"
-            />
-            <HeroCTAButton
-              title="View Projects"
-              color="white"
-            />
+            {/* actions */}
+            <div className="flex gap-x-10 gap-y-2 sm:gap-14 w-full sm:w-max flex-wrap mt-5">
+              <HeroCTAButton
+                title="Get Started"
+                color="green"
+              />
+              <HeroCTAButton
+                title="View Projects"
+                color="white"
+              />
+            </div>
           </div>
-        </div>
+        </motion.div>
         <div className="flex flex-col gap-4">
           <h2 className="text-base font-normal text-light-muted dark:text-dark-muted">
             Working with the best
